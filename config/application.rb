f243fe91d5eaeb3c3ad7fe430063dbb5579aea39
nezccommand <nezu9000@gmail.com>
2025-04-11 02:17:10 +0900

    fix:データベース関連の記述

--- config/application.rb
+++ config/application.rb
@@ -12,12 +12,10 @@ module Myapp
     config.load_defaults 7.2
     config.api_only = true
     config.active_support.to_time_preserves_timezone = :zone
-
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w[assets tasks])
-
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
