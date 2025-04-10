ebfed54344c1cc5538729eefce360c5836fb0830
nezccommand <nezu9000@gmail.com>
2025-04-11 01:51:08 +0900

    add:twitterシェア機能の実装

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  def rare_gorilla(results)
+    rarity = {
+      "？？？" => 6,
+      "金" => 5,
+      "銀" => 4,
+      "銅" => 3,
+      "3匹" => 2,
+      "1匹" => 1
+    }
+
+    results.max_by{ |a| rarity[a['result']] }['result']
+    # resultの中身をrarityに変換したものを比較し、最大のものを取り出し、最後に文字列に戻している
+  end
 end
